4dbc98f39e3b2f84c056a2871e6fbd440e32d267
Mark Whitaker <mark@mainwave.co.uk>
2020-03-22 09:28:45 +0000

    Improved HtmlService unit tests

--- SaturdayQuizWeb.UnitTests/Services/HtmlServiceTest.cs
+++ SaturdayQuizWeb.UnitTests/Services/HtmlServiceTest.cs
@@ -21,191 +21,129 @@ namespace SaturdayQuizWeb.UnitTests.Services
         }
         
         [Test]
-        public void TestQuestionCount()
+        public void GivenQuizIsLoaded_WhenQuestionCount_ThenCountIs15()
         {
-            Assert.AreEqual(15, _questions.Count);
+            Assert.That(_questions.Count, Is.EqualTo(15));
         }
 
-        [Test]
-        public void TestQuestion1()
+        [TestCase(0, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 4)]
+        [TestCase(4, 5)]
+        [TestCase(5, 6)]
+        [TestCase(6, 7)]
+        [TestCase(7, 8)]
+        [TestCase(8, 9)]
+        [TestCase(9, 10)]
+        [TestCase(10, 11)]
+        [TestCase(11, 12)]
+        [TestCase(12, 13)]
+        [TestCase(13, 14)]
+        [TestCase(14, 15)]
+        public void GivenQuizIsLoaded_WhenQuestionNumber_ThenNumberIsCorrect(int questionIndex, int expectedQuestionNumber)
         {
-            var question = _questions[0];
-            Assert.AreEqual(1, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("Which Nazi leader died in Paddington in 1981?", question.QuestionText);
-            Assert.AreEqual("Albert Speer", question.AnswerText);
-            Assert.AreEqual("Which Nazi leader died in Paddington in 1981?", question.QuestionHtml);
-            Assert.AreEqual("Albert Speer", question.AnswerHtml);
+            Assert.That(_questions[questionIndex].Number, Is.EqualTo(expectedQuestionNumber));
         }
 
-        [Test]
-        public void TestQuestion2()
+        [TestCase(0, QuestionType.Normal)]
+        [TestCase(1, QuestionType.Normal)]
+        [TestCase(2, QuestionType.Normal)]
+        [TestCase(3, QuestionType.Normal)]
+        [TestCase(4, QuestionType.Normal)]
+        [TestCase(5, QuestionType.Normal)]
+        [TestCase(6, QuestionType.Normal)]
+        [TestCase(7, QuestionType.Normal)]
+        [TestCase(8, QuestionType.WhatLinks)]
+        [TestCase(9, QuestionType.WhatLinks)]
+        [TestCase(10, QuestionType.WhatLinks)]
+        [TestCase(11, QuestionType.WhatLinks)]
+        [TestCase(12, QuestionType.WhatLinks)]
+        [TestCase(13, QuestionType.WhatLinks)]
+        [TestCase(14, QuestionType.WhatLinks)]
+        public void GivenQuizIsLoaded_WhenQuestionType_ThenTypeIsCorrect(int questionIndex, QuestionType expectedQuestionType)
         {
-            var question = _questions[1];
-            Assert.AreEqual(2, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("What are produced at La Masia & La Fábrica?", question.QuestionText);
-            Assert.AreEqual("Footballers (academies of Barcelona and Real Madrid)", question.AnswerText);
-            // Check that normal HTML tags are allowed
-            Assert.AreEqual("What are produced at <i>La Masia</i> &amp; <i>La Fábrica</i>?", question.QuestionHtml);
-            // Check that <a> tags are removed
-            Assert.AreEqual("Footballers (academies of Barcelona and Real Madrid)", question.AnswerHtml);
+            Assert.That(_questions[questionIndex].Type, Is.EqualTo(expectedQuestionType));
         }
 
-        [Test]
-        public void TestQuestion3()
+        [TestCase(0, "Which Nazi leader died in Paddington in 1981?")]
+        [TestCase(1, "What are produced at La Masia & La Fábrica?")]
+        [TestCase(2, "In publishing, what does ISBN stand for?")]
+        [TestCase(3, "Adopted in 1625, what symbol is the Dannebrog?")]
+        [TestCase(4, "Gabriele Münter was a founder member of what expressionist group?")]
+        [TestCase(5, "What was nicknamed the Honourable John Company?")]
+        [TestCase(6, "Which country separates Guyana and French Guiana?")]
+        [TestCase(7, "In what novel is Constance unhappily married to Sir Clifford?")]
+        [TestCase(8, "Asgard and Midgard, in the form of a rainbow?")]
+        [TestCase(9, "Singer O’Dowd; outlaw McCarty; slugger Ruth; bank robber Nelson?")]
+        [TestCase(10, "Statant; sejant; rampant; passant; dormant?")]
+        [TestCase(11, "Victoria Embankment; Cardiff City Hall; Colchester station?")]
+        [TestCase(12, "Khumbu icefall; Kangshung face; Hornbein couloir; Hillary step?")]
+        [TestCase(13, "Prayers at 6am; 0 degrees longitude; 2, 3, 5, 7, etc?")]
+        [TestCase(14, "Prince of Morocco (Au); Prince of Arragon (Ag); Bassanio (Pb)?")]
+        public void GivenQuizIsLoaded_WhenQuestionText_ThenTextIsCorrect(int questionIndex, string expectedQuestionText)
         {
-            var question = _questions[2];
-            Assert.AreEqual(3, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("In publishing, what does ISBN stand for?", question.QuestionText);
-            Assert.AreEqual("International Standard Book Number", question.AnswerText);
-            Assert.AreEqual("In publishing, what does ISBN stand for?", question.QuestionHtml);
-            Assert.AreEqual("International Standard Book Number", question.AnswerHtml);
+            Assert.That(_questions[questionIndex].QuestionText, Is.EqualTo(expectedQuestionText));
         }
 
-        [Test]
-        public void TestQuestion4()
+        [TestCase(0, "Which Nazi leader died in Paddington in 1981?")]
+        [TestCase(1, "What are produced at <i>La Masia</i> &amp; <i>La Fábrica</i>?")]
+        [TestCase(2, "In publishing, what does ISBN stand for?")]
+        [TestCase(3, "Adopted in 1625, what symbol is the Dannebrog?")]
+        [TestCase(4, "Gabriele Münter was a founder member of what expressionist group?")]
+        [TestCase(5, "What was nicknamed the Honourable John Company?")]
+        [TestCase(6, "Which country separates Guyana and French Guiana?")]
+        [TestCase(7, "In what novel is Constance unhappily married to Sir Clifford?")]
+        [TestCase(8, "Asgard and Midgard, in the form of a rainbow?")]
+        [TestCase(9, "Singer O’Dowd; outlaw McCarty; slugger Ruth; bank robber Nelson?")]
+        [TestCase(10, "Statant; sejant; rampant; passant; dormant?")]
+        [TestCase(11, "Victoria Embankment; Cardiff City Hall; Colchester station?")]
+        [TestCase(12, "Khumbu icefall; Kangshung face; Hornbein couloir; Hillary step?")]
+        [TestCase(13, "Prayers at 6am; 0 degrees longitude; 2, 3, 5, 7, etc?")]
+        [TestCase(14, "Prince of Morocco (Au); Prince of Arragon (Ag); Bassanio (Pb)?")]
+        public void GivenQuizIsLoaded_WhenQuestionHtml_ThenHtmlIsCorrect(int questionIndex, string expectedQuestionHtml)
         {
-            var question = _questions[3];
-            Assert.AreEqual(4, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("Adopted in 1625, what symbol is the Dannebrog?", question.QuestionText);
-            Assert.AreEqual("Danish flag", question.AnswerText);
-            Assert.AreEqual("Adopted in 1625, what symbol is the Dannebrog?", question.QuestionHtml);
-            Assert.AreEqual("Danish flag", question.AnswerHtml);
+            Assert.That(_questions[questionIndex].QuestionHtml, Is.EqualTo(expectedQuestionHtml));
         }
 
-        [Test]
-        public void TestQuestion5()
+        [TestCase(0, "Albert Speer")]
+        [TestCase(1, "Footballers (academies of Barcelona and Real Madrid)")]
+        [TestCase(2, "International Standard Book Number")]
+        [TestCase(3, "Danish flag")]
+        [TestCase(4, "Der Blaue Reiter (Blue Rider)")]
+        [TestCase(5, "East India Company")]
+        [TestCase(6, "Suriname")]
+        [TestCase(7, "Lady Chatterley’s Lover")]
+        [TestCase(8, "Bifrost (bridge in Norse myth, linking gods’ realm and Earth)")]
+        [TestCase(9, "Young nicknames: Boy George; Billy the Kid; Babe Ruth; Baby Face Nelson")]
+        [TestCase(10, "Attitudes of animals in heraldry: standing; sitting; rearing; walking; lying down")]
+        [TestCase(11, "Statues of Boudicca")]
+        [TestCase(12, "Parts of Mount Everest")]
+        [TestCase(13, "Prime: canonical hour of prayer; prime meridian; prime numbers")]
+        [TestCase(14, "Caskets chosen by Portia’s suitors in The Merchant Of Venice: gold; silver; lead")]
+        public void GivenQuizIsLoaded_WhenAnswerText_ThenTextIsCorrect(int questionIndex, string expectedAnswerText)
         {
-            var question = _questions[4];
-            Assert.AreEqual(5, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("Gabriele Münter was a founder member of what expressionist group?", question.QuestionText);
-            Assert.AreEqual("Der Blaue Reiter (Blue Rider)", question.AnswerText);
-            Assert.AreEqual("Gabriele Münter was a founder member of what expressionist group?", question.QuestionHtml);
-            Assert.AreEqual("Der Blaue Reiter (Blue Rider)", question.AnswerHtml);
+            Assert.That(_questions[questionIndex].AnswerText, Is.EqualTo(expectedAnswerText));
         }
 
-        [Test]
-        public void TestQuestion6()
+        [TestCase(0, "Albert Speer")]
+        [TestCase(1, "Footballers (academies of <i>Barcelona</i> and <i>Real Madrid</i>)")]
+        [TestCase(2, "International Standard Book Number")]
+        [TestCase(3, "Danish flag")]
+        [TestCase(4, "Der Blaue Reiter (Blue Rider)")]
+        [TestCase(5, "East India Company")]
+        [TestCase(6, "Suriname")]
+        [TestCase(7, "Lady Chatterley’s Lover")]
+        [TestCase(8, "Bifrost (bridge in Norse myth, linking gods’ realm and Earth)")]
+        [TestCase(9, "Young nicknames: Boy George; Billy the Kid; Babe Ruth; Baby Face Nelson")]
+        [TestCase(10, "Attitudes of animals in heraldry: standing; sitting; rearing; walking; lying down")]
+        [TestCase(11, "Statues of Boudicca")]
+        [TestCase(12, "Parts of Mount Everest")]
+        [TestCase(13, "Prime: canonical hour of prayer; prime meridian; prime numbers")]
+        [TestCase(14, "Caskets chosen by Portia’s suitors in The Merchant Of Venice: gold; silver; lead")]
+        public void GivenQuizIsLoaded_WhenAnswerHtml_ThenHtmlIsCorrect(int questionIndex, string expectedAnswerHtml)
         {
-            var question = _questions[5];
-            Assert.AreEqual(6, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("What was nicknamed the Honourable John Company?", question.QuestionText);
-            Assert.AreEqual("East India Company", question.AnswerText);
-            Assert.AreEqual("What was nicknamed the Honourable John Company?", question.QuestionHtml);
-            Assert.AreEqual("East India Company", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion7()
-        {
-            var question = _questions[6];
-            Assert.AreEqual(7, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("Which country separates Guyana and French Guiana?", question.QuestionText);
-            Assert.AreEqual("Suriname", question.AnswerText);
-            Assert.AreEqual("Which country separates Guyana and French Guiana?", question.QuestionHtml);
-            Assert.AreEqual("Suriname", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion8()
-        {
-            var question = _questions[7];
-            Assert.AreEqual(8, question.Number);
-            Assert.AreEqual(QuestionType.Normal, question.Type);
-            Assert.AreEqual("In what novel is Constance unhappily married to Sir Clifford?", question.QuestionText);
-            Assert.AreEqual("Lady Chatterley’s Lover", question.AnswerText);
-            Assert.AreEqual("In what novel is Constance unhappily married to Sir Clifford?", question.QuestionHtml);
-            Assert.AreEqual("Lady Chatterley’s Lover", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion9()
-        {
-            var question = _questions[8];
-            Assert.AreEqual(9, question.Number);
-            Assert.AreEqual(QuestionType.WhatLinks, question.Type);
-            Assert.AreEqual("Asgard and Midgard, in the form of a rainbow?", question.QuestionText);
-            Assert.AreEqual("Bifrost (bridge in Norse myth, linking gods’ realm and Earth)", question.AnswerText);
-            Assert.AreEqual("Asgard and Midgard, in the form of a rainbow?", question.QuestionHtml);
-            Assert.AreEqual("Bifrost (bridge in Norse myth, linking gods’ realm and Earth)", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion10()
-        {
-            var question = _questions[9];
-            Assert.AreEqual(10, question.Number);
-            Assert.AreEqual(QuestionType.WhatLinks, question.Type);
-            Assert.AreEqual("Singer O’Dowd; outlaw McCarty; slugger Ruth; bank robber Nelson?", question.QuestionText);
-            Assert.AreEqual("Young nicknames: Boy George; Billy the Kid; Babe Ruth; Baby Face Nelson", question.AnswerText);
-            Assert.AreEqual("Singer O’Dowd; outlaw McCarty; slugger Ruth; bank robber Nelson?", question.QuestionHtml);
-            Assert.AreEqual("Young nicknames: Boy George; Billy the Kid; Babe Ruth; Baby Face Nelson", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion11()
-        {
-            var question = _questions[10];
-            Assert.AreEqual(11, question.Number);
-            Assert.AreEqual(QuestionType.WhatLinks, question.Type);
-            Assert.AreEqual("Statant; sejant; rampant; passant; dormant?", question.QuestionText);
-            Assert.AreEqual("Attitudes of animals in heraldry: standing; sitting; rearing; walking; lying down", question.AnswerText);
-            Assert.AreEqual("Statant; sejant; rampant; passant; dormant?", question.QuestionHtml);
-            Assert.AreEqual("Attitudes of animals in heraldry: standing; sitting; rearing; walking; lying down", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion12()
-        {
-            var question = _questions[11];
-            Assert.AreEqual(12, question.Number);
-            Assert.AreEqual(QuestionType.WhatLinks, question.Type);
-            Assert.AreEqual("Victoria Embankment; Cardiff City Hall; Colchester station?", question.QuestionText);
-            Assert.AreEqual("Statues of Boudicca", question.AnswerText);
-            Assert.AreEqual("Victoria Embankment; Cardiff City Hall; Colchester station?", question.QuestionHtml);
-            Assert.AreEqual("Statues of Boudicca", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion13()
-        {
-            var question = _questions[12];
-            Assert.AreEqual(13, question.Number);
-            Assert.AreEqual(QuestionType.WhatLinks, question.Type);
-            Assert.AreEqual("Khumbu icefall; Kangshung face; Hornbein couloir; Hillary step?", question.QuestionText);
-            Assert.AreEqual("Parts of Mount Everest", question.AnswerText);
-            Assert.AreEqual("Khumbu icefall; Kangshung face; Hornbein couloir; Hillary step?", question.QuestionHtml);
-            Assert.AreEqual("Parts of Mount Everest", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion14()
-        {
-            var question = _questions[13];
-            Assert.AreEqual(14, question.Number);
-            Assert.AreEqual(QuestionType.WhatLinks, question.Type);
-            Assert.AreEqual("Prayers at 6am; 0 degrees longitude; 2, 3, 5, 7, etc?", question.QuestionText);
-            Assert.AreEqual("Prime: canonical hour of prayer; prime meridian; prime numbers", question.AnswerText);
-            Assert.AreEqual("Prayers at 6am; 0 degrees longitude; 2, 3, 5, 7, etc?", question.QuestionHtml);
-            Assert.AreEqual("Prime: canonical hour of prayer; prime meridian; prime numbers", question.AnswerHtml);
-        }
-
-        [Test]
-        public void TestQuestion15()
-        {
-            var question = _questions[14];
-            Assert.AreEqual(15, question.Number);
-            Assert.AreEqual(QuestionType.WhatLinks, question.Type);
-            Assert.AreEqual("Prince of Morocco (Au); Prince of Arragon (Ag); Bassanio (Pb)?", question.QuestionText);
-            Assert.AreEqual("Caskets chosen by Portia’s suitors in The Merchant Of Venice: gold; silver; lead", question.AnswerText);
-            Assert.AreEqual("Prince of Morocco (Au); Prince of Arragon (Ag); Bassanio (Pb)?", question.QuestionHtml);
-            Assert.AreEqual("Caskets chosen by Portia’s suitors in The Merchant Of Venice: gold; silver; lead", question.AnswerHtml);
+            Assert.That(_questions[questionIndex].AnswerHtml, Is.EqualTo(expectedAnswerHtml));
         }
     }
 }
